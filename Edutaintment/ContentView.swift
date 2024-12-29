cdb67f748202efe12c2f23bfc908110d9bf8de9d
David-Brnb <321171197@ciencias.unam.mx>
2024-12-28 21:46:40 -0600

    Initial Commit

---
+++ Edutaintment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Edutaintment
+//
+//  Created by Leoni Bernabe on 28/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Edutaintment/EdutaintmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutaintmentApp.swift
+//  Edutaintment
+//
+//  Created by Leoni Bernabe on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutaintmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
